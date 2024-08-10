f740e4d68583ab077c46d69f8c104ccea7e34bd2
nipun-das <nipundas02@gmail.com>
2024-08-10 20:49:31 +0530

    Initial commits

---
+++ kafka-wikimedia-stream/src/main/java/com/kafka_wikimedia_stream/kafka_wikimedia_stream/KafkaWikimediaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.kafka_wikimedia_stream.kafka_wikimedia_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWikimediaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWikimediaStreamApplication.class, args);
+	}
+
+}
---
+++ kafka-wikimedia-stream/src/test/java/com/kafka_wikimedia_stream/kafka_wikimedia_stream/KafkaWikimediaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_wikimedia_stream.kafka_wikimedia_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWikimediaStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
